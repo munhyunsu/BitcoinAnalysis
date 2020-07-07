94e3779ef45dd7f665102a5aacffc16d5e2a512f
munhyunsu <munhyunsu@gmail.com>
2020-07-08 07:06:29 +0900

    time log

--- BitcoinBlockSampler/build_graph.c
+++ BitcoinBlockSampler/build_graph.c
@@ -1,9 +1,13 @@
+#include <time.h>
+
 #include <igraph.h>
 
 int main(int argc, char* argv[]) {
     igraph_t graph;
     FILE *input;
 
+    time_t stime, etime;
+    time(&stime);
     if(argc <= 1) {
         return 0;
     }
@@ -13,24 +17,29 @@ int main(int argc, char* argv[]) {
     }
     igraph_read_graph_edgelist(&graph, input, 0, 0);
     fclose(input);
-    printf("Graph load complete with %i vertices and %i edges\n",
-             igraph_vcount(&graph), igraph_ecount(&graph));
+    time(&etime);
+    printf("[%ld] Graph load complete with %i vertices and %i edges\n",
+             (etime-stime), igraph_vcount(&graph), igraph_ecount(&graph));
 
+    time(&stime);
     igraph_vector_t membership, degree;
     igraph_vector_init(&membership, igraph_vcount(&graph));
     igraph_vector_init(&degree, igraph_vcount(&graph));
     igraph_degree(&graph, &degree, igraph_vss_all(), IGRAPH_ALL, 1);
     igraph_integer_t nb_clusters;
     igraph_real_t quality;
-    printf("Initialize for community detection complete\n");
+    time(&etime);
+    printf("[%ld] Initialize for community detection complete\n",
+              (etime-stime));
 
+    time(&stime);
     igraph_community_leiden(&graph, 
                             NULL, &degree, 
                             1.0/(2*igraph_ecount(&graph)), 0.01, 0, 
                             &membership, &nb_clusters, &quality);
-
-    printf("Leiden found %i clusters using modularity, quality is %.4f.\n",
-             nb_clusters, quality);
+    time(&etime);
+    printf("[%ld] Leiden found %i clusters using modularity, quality is %.4f.\n",
+             (etime-stime), nb_clusters, quality);
     igraph_vector_print(&membership);
     printf("\n");
 
