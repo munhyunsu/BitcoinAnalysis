1de4de3b75f824addc8bac265cb830f996cdcf29
munhyunsu <munhyunsu@gmail.com>
2020-07-27 10:22:10 +0900

    remove testing

--- BitcoinBlockSampler/db_manager.py
+++ BitcoinBlockSampler/db_manager.py
@@ -35,21 +35,8 @@ QUERY['CREATE_TXOUT_TABLE'] = '''
       n INTEGER,
       addr INTEGER,
       UNIQUE (tx, n, addr));'''
-
-QUERY['CREATE_BLKID_TABLE2'] = '''
-    CREATE TABLE IF NOT EXISTS BlkID (
-      id INTEGER,
-      blkhash TEXT NOT NULL UNIQUE);'''
-QUERY['CREATE_TXID_TABLE2'] = '''
-    CREATE TABLE IF NOT EXISTS TxID (
-      txid TEXT NOT NULL UNIQUE);'''
-QUERY['CREATE_ADDRID_TABLE2'] = '''
-    CREATE TABLE IF NOT EXISTS AddrID (
-      addr TEXT NOT NULL UNIQUE);'''
-
 QUERY['SELECT_MAX_BLKID'] = '''
     SELECT MAX(id) FROM BlkID;'''
-
 QUERY['INSERT_META'] = '''
     INSERT OR IGNORE INTO Meta (
       key, value) VALUES (
@@ -66,7 +53,6 @@ QUERY['INSERT_ADDRID'] = '''
     INSERT OR IGNORE INTO AddrID (
       addr) VALUES (
       ?);'''
-
 QUERY['UPDATE_META'] = '''
     UPDATE Meta SET value=? 
     WHERE key=?;'''
@@ -96,60 +82,6 @@ class DBBuilder(object):
                   'CREATE_BLKID_TABLE',
                   'CREATE_TXID_TABLE',
                   'CREATE_ADDRID_TABLE']:
-            self.cur.execute(QUERY[q])
-        self.commit()
-    
-    def select(self, query, *args):
-        self.cur.execute(QUERY[query], *args)
-        value = self.cur.fetchone()
-        if value is not None:
-            value = value[0]
-        return value
-
-    def selectall(self, query, *args):
-        self.cur.execute(QUERY[query], *args)
-        return self.cur.fetchall()
-
-    def insert(self, query, *args):
-        self.cur.execute(QUERY[query], *args)
-        self.commit()
-
-    def insertmany(self, query, *args):
-        self.cur.executemany(QUERY[query], *args)
-
-    def begin(self):
-        self.cur.execute('BEGIN TRANSACTION;')
-
-    def commit(self):
-        self.cur.execute('COMMIT TRANSACTION;')
-
-    def close(self):
-        self.conn.close()
-
-class DBBuilderTest(object):
-    def __init__(self, dbtype: str, dbpath: str):
-        self.dbpath = dbpath
-        self.conn = sqlite3.connect(self.dbpath)
-        self.cur = self.conn.cursor()
-        self.cur.execute('PRAGMA synchronous = NORMAL;')
-        self.cur.execute('PRAGMA journal_mode = WAL;')
-        self.conn.commit()
-        dbtype = dbtype.lower()
-        if dbtype == 'index':
-            self._create_table_index()
-        elif dbtype == 'core':
-            self._create_table_core()
-        elif dbtype == 'util':
-            self._create_table_util()
-        else:
-            assert Exception('[Error] dbtype is not one of [index, core, util]')
-
-    def _create_table_index(self):
-        self.begin()
-        for q in ['CREATE_META_TABLE',
-                  'CREATE_BLKID_TABLE2',
-                  'CREATE_TXID_TABLE2',
-                  'CREATE_ADDRID_TABLE2']:
             self.cur.execute(QUERY[q])
         self.commit()
     
