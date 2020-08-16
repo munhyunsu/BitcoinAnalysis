5786d722922d57184ce35fea8b8dd25d8a43bd9b
munhyunsu <munhyunsu@gmail.com>
2020-08-16 15:33:32 +0900

    core resume

--- BitcoinBlockSampler/main_db_builder.py
+++ BitcoinBlockSampler/main_db_builder.py
@@ -171,7 +171,7 @@ def main():
             dbb.cur.execute(f'''CREATE INDEX idx_BlkTx_2 ON BlkTx(tx);''')
             dbb.cur.execute(f'''CREATE INDEX idx_TxIn_3_4 ON TxIn(ptx, pn);''')
             dbb.cur.execute(f'''CREATE INDEX idx_TxOut_3 ON TxOut(addr);''')
-        dbb.commit()
+            dbb.commit()
         if DEBUG:
             print(f'All job completed {start_height} to {end_height} during {time.time()-stime}')
         INDEX.close()
