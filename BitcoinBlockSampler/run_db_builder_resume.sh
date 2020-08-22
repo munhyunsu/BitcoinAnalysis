8225898e8a9767050ea6ef5657f54a82d6c7edda
munhyunsu <munhyunsu@gmail.com>
2020-08-22 19:43:23 +0900

    Edge database resume

--- BitcoinBlockSampler/run_db_builder_resume.sh
+++ BitcoinBlockSampler/run_db_builder_resume.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 
-python3 python3 main_db_builder.py --debug --type index --resume
-python3 python3 main_db_builder.py --debug --type core --index dbv3-index.db --resume
+CMD='SELECT MAX(id) FROM BlkID;'
+BHEIGHT=`sqlite3 dbv3-index.db "${CMD}"`
+echo "Resume from ${BHEIGHT}"
 
+python3 main_db_builder.py --debug --type index --resume
+python3 main_db_builder.py --debug --type core --index dbv3-index.db --resume
+
+echo "Update Edge Database"
+CMD="ATTACH DATABASE './dbv3-index.db' AS DBINDEX;
+ATTACH DATABASE './dbv3-core.db' AS DBCORE;
+
+INSERT OR IGNORE INTO Edge (tx, src, dst, btc)
+SELECT TXI.tx, TXI.addr, TXO.addr, TXO.btc
+FROM (SELECT DBCORE.TxIn.tx AS tx, DBCORE.TxIn.n AS n, 
+             DBCORE.TxOut.addr AS addr, DBCORE.TxOut.btc AS btc
+      FROM DBCORE.TxIn
+      INNER JOIN DBCORE.TxOut ON DBCORE.TxOut.tx = DBCORE.TxIn.ptx AND 
+                                 DBCORE.TxOut.n = DBCORE.TxIn.pn
+      WHERE DBCORE.TxIn.tx IN (SELECT DBCORE.BlkTx.tx
+                               FROM DBCORE.BlkTx
+                               WHERE DBCORE.BlkTx.blk >= ${BHEIGHT})) AS TXI
+INNER JOIN (SELECT DBCORE.TxOut.tx AS tx, DBCORE.TxOut.n AS n, 
+                   DBCORE.TxOut.addr AS addr, DBCORE.TxOut.btc AS btc
+            FROM DBCORE.TxOut
+            WHERE DBCORE.TxOut.tx IN (SELECT DBCORE.BlkTx.tx
+                                      FROM DBCORE.BlkTx
+                                      WHERE DBCORE.BlkTx.blk >= ${BHEIGHT})
+           ) AS TXO ON TXO.tx = TXI.tx;"
+sqlite3 dbv3-util.db "${CMD}"
+echo "Resume complete"
