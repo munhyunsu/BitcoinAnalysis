29be133a14426d1fea26290152f2829608d3f00c
munhyunsu <munhyunsu@gmail.com>
2020-09-19 22:13:29 +0900

    parameter

--- BitcoinBlockSampler/run_db_builder_init.sh
+++ BitcoinBlockSampler/run_db_builder_init.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
-python3 main_db_builder.py --debug --type index --pagesize 65536 --cachesize 6553600
-python3 main_db_builder.py --debug --type core --index dbv3-index.db --pagesize 65536 --cachesize 6553600
+python3 main_db_builder.py --debug --type index --pagesize 65536 --cachesize -6553600
+python3 main_db_builder.py --debug --type core --index dbv3-index.db --pagesize 65536 --cachesize -6553600
 
 echo "Update Edge Database"
-CMD="ATTACH DATABASE './dbv3-index.db' AS DBINDEX;
+CMD="
+PRAGMA synchronous = OFF;
+PRAGMA journal_mode = OFF;
+
+PRAGMA cache_size = -6553600;
+PRAGMA page_size = 65536;
+VACUUM;
+
+ATTACH DATABASE './dbv3-index.db' AS DBINDEX;
 ATTACH DATABASE './dbv3-core.db' AS DBCORE;
+
+CREATE TABLE IF NOT EXISTS Edge (
+    tx INTEGER NOT NULL,
+    src INTEGER NOT NULL,
+    dst INTEGER NOT NULL,
+    btc REAL NOT NULL,
+    UNIQUE (tx, src, dst, btc));
 
 INSERT OR IGNORE INTO Edge (tx, src, dst, btc)
 SELECT TXI.tx, TXI.addr, TXO.addr, TXO.btc
@@ -17,6 +32,18 @@ FROM (SELECT DBCORE.TxIn.tx AS tx, DBCORE.TxIn.n AS n,
 INNER JOIN (SELECT DBCORE.TxOut.tx AS tx, DBCORE.TxOut.n AS n, 
                    DBCORE.TxOut.addr AS addr, DBCORE.TxOut.btc AS btc
             FROM DBCORE.TxOut
-           ) AS TXO ON TXO.tx = TXI.tx;"
+           ) AS TXO ON TXO.tx = TXI.tx;
+           
+CREATE INDEX idx_Edge_1 ON Edge(tx);
+CREATE INDEX idx_Edge_2 ON Edge(src);
+CREATE INDEX idx_Edge_3 ON Edge(dst);
+
+PRAGMA cache_size = -2000;
+PRAGMA page_size = 4096;
+VACUUM;
+
+PRAGMA synchronous = NORMAL;
+PRAGMA journal_mode = WAL;
+"
 sqlite3 dbv3-util.db "${CMD}"
-echo "Resume complete"
+echo "Building complete"
