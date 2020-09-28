0a1c6b2b244f8f6791a75ca5c66bae328959bb16
munhyunsu <munhyunsu@gmail.com>
2020-09-28 23:14:04 +0900

    timezone indicator

--- BitcoinBlockSampler/time_manager.py
+++ BitcoinBlockSampler/time_manager.py
@@ -4,8 +4,8 @@ import datetime
 tz_seoul = datetime.timezone(datetime.timedelta(hours=9))
 tz_utc = datetime.timezone(datetime.timedelta())
 
-def get_time(timestamp):
-    return datetime.datetime.fromtimestamp(timestamp, tz=tz_seoul)
+def get_time(timestamp, tz=tz_utc):
+    return datetime.datetime.fromtimestamp(timestamp, tz=tz)
 
 
 def get_unixtime(timestr):
