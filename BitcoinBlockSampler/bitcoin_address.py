ced1c6032d0ce1bc43f2d3cf2233048fd575f6bb
munhyunsu <munhyunsu@gmail.com>
2020-09-29 09:11:43 +0900

    address info

--- BitcoinBlockSampler/bitcoin_address.py
+++ BitcoinBlockSampler/bitcoin_address.py
@@ -1,3 +1,12 @@
+import time_manager
+from db_manager import QUERY
+
+
 class Address(object):
-    def __init__(self):
+    def __init__(self, conn):
+        self.conn = conn
+        self.cur = conn.cursor()
+        self.info = dict()
+    
+    def _get_info():
         pass
