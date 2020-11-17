5255b8320281ee6aac1901378d8599b8d122e8f6
munhyunsu <munhyunsu@gmail.com>
2020-11-18 08:42:53 +0900

    UnionFind interface

--- BitcoinBlockSampler/data_structure.py
+++ BitcoinBlockSampler/data_structure.py
@@ -3,4 +3,10 @@ import numpy as np
 
 class UnionFind(object):
     def __init__(self):
-        self.par = np.
+        pass
+
+    def find(self, o):
+        pass
+
+    def union(self, o1, o2):
+        pass
