f8ea9fdf0776623b03ec035e1ad1492dd974baec
munhyunsu <munhyunsu@gmail.com>
2020-12-13 20:05:22 +0900

    Transactions

--- BitcoinBlockSampler/main_build_graph_by_address.py
+++ BitcoinBlockSampler/main_build_graph_by_address.py
@@ -11,6 +11,39 @@ DEBUG = None
 STIME = time.time()
 CONN = CUR = None
 
+Q = dict()
+Q['Addr2ID'] = '''
+SELECT DBINDEX.AddrID.id 
+FROM DBINDEX.AddrID
+WHERE DBINDEX.AddrID.addr = ?;'''
+Q['TxInByAddr'] = '''
+SELECT DBCORE.TxIn.tx
+FROM DBCORE.TxIn
+INNER JOIN DBCORE.TxOut ON DBCORE.TxOut.tx = DBCORE.TxIn.ptx AND
+                           DBCORE.TxOut.n = DBCORE.TxIn.pn
+WHERE DBCORE.TxOut.addr = ?;'''
+Q['TxOutByAddr'] = '''
+SELECT DBCORE.TxOut.tx
+FROM DBCORE.TxOut
+WHERE DBCORE.TxOut.addr = ?;'''
+Q['AddrByTxIn'] = '''
+SELECT DBCORE.TxOut.addr
+FROM DBCORE.TxIn
+INNER JOIN DBCORE.TxOut ON DBCORE.TxOut.tx = DBCORE.TxIn.ptx AND
+                           DBCORE.TxOut.n = DBCORE.TxIn.pn
+WHERE DBCORE.TxIn.tx = ?;'''
+Q['AddrByTxOut'] = '''
+SELECT DBCORE.TxOut.addr
+FROM DBCORE.TxOut
+WHERE DBCORE.TxOut.tx = ?;'''
+Q['EdgeByAddr'] = '''
+SELECT DBUtil.Edge.src AS src, DBUtil.Edge.dst AS dst, 
+       SUM(DBUtil.Edge.btc) AS btc, COUNT(DBUtil.Edge.tx) AS cnt
+FROM DBUtil.Edge
+WHERE DBUtil.Edge.src = ?
+   OR DBUtil.Edge.dst = ?
+GROUP BY DBUtil.Edge.src, DBUtil.Edge.dst;'''
+
 
 def main():
     # Opening
@@ -24,6 +57,18 @@ def main():
     CUR.execute(f'''ATTACH DATABASE '{FLAGS.core}' AS DBCORE;''')
     CUR.execute(f'''ATTACH DATABASE '{FLAGS.util}' AS DBUTIL;''')
     CONN.commit()
+    
+    # Address to ID
+    cur.execute(Q['Addr2ID'], (FLAGS.input,))
+    addrid = cur.fetchone()[0]
+    
+    # Transactions which beloging target address
+    txes = set()
+    for result in cur.execute(Q['TxInByAddr'], (ADDRID,)):
+        txes.add(result[0])
+    for result in cur.execute(Q['TxOutByAddr'], (ADDRID,)):
+        txes.add(result[0])
+    print(f'Trainsactions: {len(txes)}')
     
     # Closing
     CONN.close()
@@ -45,6 +90,8 @@ if __name__ == '__main__':
                         help='The path for core database')
     parser.add_argument('--util', type=str, required=True,
                         help='The path for util database')
+    parser.add_argument('--input', type=str, required=True,
+                        help='The target bitcoin address')
     parser.add_argument('--output', type=str, required=True,
                         help='The path for igraph file')
 
