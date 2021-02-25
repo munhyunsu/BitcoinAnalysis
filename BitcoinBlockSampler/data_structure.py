ac0158ff721ed4e6d754cccf5f2503a015f96d48
munhyunsu <munhyunsu@gmail.com>
2021-02-26 08:40:14 +0900

    Use only parent

--- BitcoinBlockSampler/data_structure.py
+++ BitcoinBlockSampler/data_structure.py
@@ -4,10 +4,9 @@ import numpy as np
 class UnionFind(object):
     def __init__(self, size=0):
         self.parent = np.full(size+1, range(size+1), dtype=np.int32)
-        self.rank = np.full(size+1, 0, dtype=np.int32)
     
     def __repr__(self):
-        return f'Parent: {self.parent}\nRank: {self.rank}'
+        return f'Parent: {self.parent}'
 
     def find(self, o):
         while o != self.parent[o]:
@@ -20,17 +19,16 @@ class UnionFind(object):
         xroot = self.find(x)
         yroot = self.find(y)
         if xroot != yroot:
-            if self.rank[xroot] < self.rank[yroot]:
-                self.parent[xroot] = yroot
-            elif self.rank[xroot] > self.rank[yroot]:
+            if self.parent[xroot] < self.parent[yroot]:
                 self.parent[yroot] = xroot
+            elif self.parent[xroot] > self.parent[yroot]:
+                self.parent[xroot] = yroot
             else:
                 low = min(xroot, yroot)
                 high = max(xroot, yroot)
                 self.parent[high] = low
-                self.rank[low] = self.rank[low] + 1
 
-    
+
 if __name__ == '__main__':
     uf = UnionFind(10)
     print(uf)
