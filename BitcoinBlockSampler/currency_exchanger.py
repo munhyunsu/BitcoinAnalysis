02a7803eea6b264b710fd62ca91abf3c08d72b95
munhyunsu <munhyunsu@gmail.com>
2021-07-11 10:42:35 +0900

    create db

--- BitcoinBlockSampler/currency_exchanger.py
+++ BitcoinBlockSampler/currency_exchanger.py
@@ -1,5 +1,6 @@
 import os
 import time
+import sqlite3
 
 FLAGS = _ = None
 DEBUG = False
@@ -9,13 +10,28 @@ STIME = None
 def prepare_conn(indexpath, corepath, utilpath, servicepath):
     conn = sqlite3.connect(':memory:')
     cur = conn.cursor()
-    cur.execute(f'''ATTACH DATABASE '{indexpath}' AS DBINDEX;''')
-    cur.execute(f'''ATTACH DATABASE '{corepath}' AS DBCORE;''')
-    cur.execute(f'''ATTACH DATABASE '{utilpath}' AS DBUTIL;''')
-    cur.execute(f'''ATTACH DATABASE '{servicepath}' AS DBSERVICE;''')
+    if indexpath is not None:
+        cur.execute(f'''ATTACH DATABASE '{indexpath}' AS DBINDEX;''')
+    if corepath is not None:
+        cur.execute(f'''ATTACH DATABASE '{corepath}' AS DBCORE;''')
+    if utilpath is not None:
+        cur.execute(f'''ATTACH DATABASE '{utilpath}' AS DBUTIL;''')
+    if servicepath is not None:
+        cur.execute(f'''ATTACH DATABASE '{servicepath}' AS DBSERVICE;''')
     conn.commit()
     
     return conn, cur
+
+
+def create_currency_db(conn, cur):
+    cur.execute('''PRAGMA journal_mode = NORMAL''')
+    cur.execute('''PRAGMA synchronous = WAL''')
+    cur.execute('''CREATE TABLE IF NOT EXISTS BTC2Dollar (
+                     unixtime INTEGER PRIMARY KEY,
+                     dollar REAL NOT NULL
+                   );''')
+    cur.commit()
+
 
 
 def main():
@@ -25,6 +41,9 @@ def main():
 
     conn, cur = prepare_conn(FLAGS.index, FLAGS.core,
                              FLAGS.util, FLAGS.service)
+
+    
+
 
 if __name__ == '__main__':
     root_path = os.path.abspath(__file__)
@@ -41,12 +60,26 @@ if __name__ == '__main__':
                         help='The path for core database')
     parser.add_argument('--util', type=str,
                         help='The path for util database')
-    parser.add_argument('--service', type=str, required=True,
+    parser.add_argument('--service', type=str, 
+    #required=True,
                         help='The path for service database')
+    parser.add_argument('--data', type=str,
+                        help='The path for BTC to Dollar ratio csv')
 
     FLAGS, _ = parser.parse_known_args()
     DEBUG = FLAGS.debug
     STIME = time.time()
+
+    if FLAGS.index is not None:
+        FLAGS.index = os.path.abspath(os.path.expanduser(FLAGS.index))
+    if FLAGS.core is not None:
+        FLAGS.core = os.path.abspath(os.path.expanduser(FLAGS.core))
+    if FLAGS.util is not None:
+        FLAGS.util = os.path.abspath(os.path.expanduser(FLAGS.util))
+    if FLAGS.service is not None:
+        FLAGS.service = os.path.abspath(os.path.expanduser(FLAGS.service))
+    if FLAGS.data is not None:
+        FLAGS.data = os.path.abspath(os.path.expanduser(FLAGS.data))
 
     main()
 
