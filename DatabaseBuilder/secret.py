8998aeb858b2c87c4f4fbb805346700427b45b33
munhyunsu <munhyunsu@gmail.com>
2021-08-18 11:04:50 +0900

    DB address and port

--- DatabaseBuilder/secret.py
+++ DatabaseBuilder/secret.py
@@ -2,3 +2,5 @@ rpc_user = '' # bitcoind JSON RPC user
 rpc_password = '' # bitcoind JSON RPC password
 db_user = '' # Mariadb user
 db_password = '' # Mariadb password
+db_address = '' # Mariadb Address
+db_port = int('') # Mariadb Port
