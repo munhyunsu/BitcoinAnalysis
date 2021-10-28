8a997cc22176944dfff710d6b9f4be9f6fbeabda
Hyun-Su, Mun <munhyunsu@gmail.com>
2021-10-28 13:37:56 +0900

    Update secret.py

--- DatabaseBuilder/secret.py
+++ DatabaseBuilder/secret.py
@@ -1,6 +1,8 @@
-rpc_user = '' # bitcoind JSON RPC user
-rpc_password = '' # bitcoind JSON RPC password
-db_user = '' # Mariadb user
-db_password = '' # Mariadb password
-db_address = '' # Mariadb Address
-db_port = int('') # Mariadb Port
+rpcuser = '' # bitcoind JSON RPC user
+rpcpassword = '' # bitcoind JSON RPC password
+dbrootpassword = '' # MariaDB root password
+dbdatabase = '' # Mariadb database name
+dbuser = '' # Mariadb user
+dbpassword = '' # Mariadb password
+dbhost = '' # Mariadb Address
+dbport = int('') # Mariadb Port
