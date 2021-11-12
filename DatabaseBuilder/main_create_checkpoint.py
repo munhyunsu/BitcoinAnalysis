bae57c90eda90903c992250ae6a7af67587aee4d
munhyunsu <munhyunsu@gmail.com>
2021-11-13 08:42:11 +0900

    remove extend list

--- DatabaseBuilder/main_create_checkpoint.py
+++ DatabaseBuilder/main_create_checkpoint.py
@@ -19,7 +19,58 @@ CONN = None
 CUR = None
 
 
-def get_block(height):
+def get_block_index(height):
+    global FLAGS
+    global DEBUG
+    global RPCM
+
+    blkids = []
+    txids = []
+    addrids = []
+
+    blockhash = None
+    block = None
+    try:
+        if RPCM is None:
+            raise BrokenPipeError
+        rpc = RPCM
+        blockhash = rpc.getblockhash(height)
+        block = rpc.getblock(blockhash, 2)
+    except (BrokenPipeError, JSONRPCException):
+        RPCM = AuthServiceProxy((f'http://{secret.rpcuser}:{secret.rpcpassword}@'
+                                 f'{secret.rpchost}:{secret.rpcport}'),
+                                timeout=FLAGS.rpctimeout)
+        rpc = RPCM
+    finally:
+        if blockhash is None:
+            blockhash = rpc.getblockhash(height)
+        if block is None:
+            block = rpc.getblock(blockhash, 2)
+    
+    blkids.append((blockhash,))
+    for btx in block['tx']:
+        tx = btx['txid']
+        txids.append((tx,))
+        for n, vout in enumerate(btx['vout'], start=0):
+            try:
+                for addr, btc in utils.addr_btc_from_vout(vout):
+                    addrids.append((addr,))
+            except Exception:
+                raise Exception(f'For debug! {tx}:{n}:{vout}')
+
+    data = {'height': height,
+            'blkids': blkids,
+            'txids': txids,
+            'addrids': addrids
+           }
+
+    if DEBUG:
+        print(f'[{int(time.time()-STIME)}] Index read: {height:<7d}', end='\r')
+
+    return data
+
+
+def get_block_data(height):
     global FLAGS
     global DEBUG
     global RPCM
@@ -40,8 +91,10 @@ def get_block(height):
         conn = CONN
         cur = CUR
     
-    list_blockhash = []
     list_blktime = []
+    list_blktx = []
+    list_txout = []
+    list_txin = []
     
     blockhash = None
     block = None
@@ -61,51 +114,45 @@ def get_block(height):
             blockhash = rpc.getblockhash(height)
         if block is None:
             block = rpc.getblock(blockhash, 2)
+
+    cur.execute('''SELECT id FROM blkid
+                     WHERE blkhash = ?;''', (blockhash,))
+    blkid = cur.fetchone()[0]
     blktime = block['time']
+    list_blktime.append((blkid, blktime))
 
     txes = []
     for btx in block['tx']:
         tx = btx['txid']
         cur.execute('''SELECT id FROM txid
                          WHERE tx = ?;''', (tx,))
-        res = cur.fetchall()
-        if len(res) != 0:
-            tx = res[0][0]
-        list_vout = []
+        txid = cur.fetchone()[0]
+        list_blktx.append((blkid, txid))
         for n, vout in enumerate(btx['vout'], start=0):
-            list_addr_btc = []
             try:
                 for addr, btc in utils.addr_btc_from_vout(vout):
                     cur.execute('''SELECT id FROM addrid
                                      WHERE addr = ?;''', (addr,))
-                    res = cur.fetchall()
-                    if len(res) != 0:
-                        addr = res[0][0]
-                    list_addr_btc.append((addr, btc))
+                    addrid = cur.fetchone()[0]
+                    list_txout.append((txid, n, addrid, btc))
             except Exception:
                 raise Exception(f'For debug! {tx}:{n}:{vout}')
-            list_vout.append(list_addr_btc)
-        list_vin = []
         for n, vin in enumerate(btx['vin'], start=0):
             if 'coinbase' in vin:
-                list_vin.append((-1, 0))
+                list_vin.append((txid, n, 0, 0))
                 continue
             ptx = vin['txid']
             cur.execute('''SELECT id FROM txid
                              WHERE tx = ?;''', (ptx,))
-            res = cur.fetchall()
-            if len(res) != 0:
-                ptx = res[0][0]
+            ptxid = cur.fetchone()[0]
             pn = vin['vout']
-            list_vin.append((ptx, pn))
-        txes.append({'tx': tx,
-                     'vout': list_vout,
-                     'vin': list_vin})
+            list_txin.append((txid, n, ptxid, pn))
     
     data = {'height': height,
-            'blkhash': blockhash,
-            'blktime': blktime,
-            'txes': txes
+            'blktime': list_blktime,
+            'blktx': list_blktx,
+            'txout': list_txout,
+            'txin': list_txin
            }
 
     if DEBUG:
@@ -147,7 +194,7 @@ def main():
                    );''')
     cur.execute('''CREATE TABLE addrid (
                      id INTEGER PRIMARY KEY,
-                     addr TEXT NOT NULL
+                     addr TEXT NOT NULL UNIQUE
                    );''')
     cur.execute('''CREATE TABLE blktime (
                      blk INTETER NOT NULL,
@@ -179,10 +226,6 @@ def main():
     cur.execute(f'''VACUUM;''')
     conn.commit()
 
-    next_blkid = 0
-    next_txid = 0
-    next_addrid = 0
-
     height_start = 0
     bestblockhash = rpc.getbestblockhash()
     bestblock = rpc.getblock(bestblockhash)
@@ -191,26 +234,14 @@ def main():
         print(f'[{int(time.time()-STIME)}] Best Block Heights: {bestblock["height"]}')
         print(f'[{int(time.time()-STIME)}] Time: {utils.gettime(bestblock["time"]).isoformat()}')
 
-    print(f'[{int(time.time()-STIME)}] Begin build database from {height_start} to {height_end}')
-    print(f'[{int(time.time()-STIME)}] with starting blkid: {next_blkid}, txid: {next_txid}, addrid: {next_addrid}')
+    print(f'[{int(time.time()-STIME)}] Begin build index database from {height_start} to {height_end}')
 
     cache_block = {}
     cache_start = 0
     cache_end = 0
-    data_blkid = []
-    map_blkid = {}
-    data_txid = []
-    map_txid = {}
-    data_addrid = []
-    map_addrid = {}
-    data_blktime = []
-    map_blktime = {}
-    data_blktx = []
-    map_blktx = {}
-    data_txout = []
-    map_txout = {}
-    data_txin = []
-    map_txin = {}
+    blkids = []
+    txids = []
+    addrids = []
     for height in range(height_start, height_end+1):
         if height not in cache_block.keys():
             cache_start = height
@@ -218,106 +249,93 @@ def main():
             if DEBUG:
                 print(f'[{int(time.time()-STIME)}] Cache update: {cache_start} ~ {cache_end-1}')
             with multiprocessing.Pool(FLAGS.process) as p:
-                results = p.map(get_block, range(cache_start, cache_end))
+                results = p.map(get_block_index, range(cache_start, cache_end))
                 for data in results:
                     cache_block[data['height']] = data
             if DEBUG:
                 print(f'[{int(time.time()-STIME)}] Cache update done')
-        if cache_block[height]['height'] != next_blkid:
+            cur.execute('''BEGIN TRANSACTION;''')
+        if cache_block[height]['height'] != height:
             print(f'Something is wrong: {block["height"]} != {next_blkid}')
             conn.close()
             sys.exit(0)
-        blockhash = cache_block[height]['blkhash']
-        blkid = next_blkid
-        next_blkid = next_blkid + 1
-        data_blkid.append((blkid, blockhash))
-        blktime = cache_block[height]['blktime']
-        data_blktime.append((blkid, blktime))
-        for txes in cache_block[height]['txes']:
-            tx = txes['tx']
-            if type(tx) == int:
-                txid = tx
-            elif tx not in map_txid.keys():
-                txid = next_txid
-                next_txid = next_txid + 1
-                map_txid[tx] = txid
-            else:
-                txid = map_txid[tx]
-            data_blktx.append((blkid, txid))
-            for n, elements in enumerate(txes['vout'], start=0):
-                for addr, btc in elements:
-                    if type(addr) == int:
-                        addrid = addr
-                    elif addr not in map_addrid.keys():
-                        addrid = next_addrid
-                        next_addrid = next_addrid + 1
-                        map_addrid[addr] = addrid
-                    else:
-                        addrid = map_addrid[addr]
-                    data_txout.append((txid, n, addrid, btc))
-            for n, elements in enumerate(txes['vin'], start=0):
-                ptx = elements[0]
-                pn = elements[1]
-                if type(ptx) == int:
-                    ptxid = ptx
-                elif ptx not in map_txid.keys():
-                    raise Exception(f'ptx not found! {tx}:{n}:{ptx}')
-                else:
-                    ptxid = map_txid[ptx]
-                data_txin.append((txid, n, ptxid, pn))
+
+        cur.executemany('''INSERT OR IGNORE INTO blkid (blkhash)
+                             VALUES (?);''', cache_block[height]['blkids'])
+        cur.executemany('''INSERT OR IGNORE INTO txid (tx)
+                             VALUES (?);''', cache_block[height]['txids'])
+        cur.executemany('''INSERT OR IGNORE INTO addrid (addr)
+                             VALUES (?);''', cache_block[height]['addrids'])
 
         if height == cache_end-1:
-            for key, value in map_blkid.items():
-                data_blkid.append((value, key))
-            data_blkid.sort(key=operator.itemgetter(0))
-            for key, value in map_txid.items():
-                data_txid.append((value, key))
-            data_txid.sort(key=operator.itemgetter(0))
-            for key, value in map_addrid.items():
-                data_addrid.append((value, key))
-            data_addrid.sort(key=operator.itemgetter(0))
-
             if DEBUG:
                 print(f'[{int(time.time()-STIME)}] Ready to transaction {height}')
             try:
-                cur.execute('''BEGIN TRANSACTION;''')
-                cur.executemany('''INSERT INTO blkid (id, blkhash)
-                                     VALUES (?, ?);''', data_blkid)
-                cur.executemany('''INSERT INTO txid (id, tx)
-                                     VALUES (?, ?);''', data_txid)
-                cur.executemany('''INSERT INTO addrid (id, addr)
-                                     VALUES (?, ?);''', data_addrid)
-                cur.executemany('''INSERT INTO blktime (blk, miningtime)
-                                     VALUES (?, ?);''', data_blktime)
-                cur.executemany('''INSERT INTO blktx (blk, tx)
-                                     VALUES (?, ?);''', data_blktx)
-                cur.executemany('''INSERT INTO txout (tx, n, addr, btc)
-                                     VALUES (?, ?, ?, ?);''', data_txout)
-                cur.executemany('''INSERT INTO txin (tx, n, ptx, pn)
-                                     VALUES (?, ?, ?, ?);''', data_txin)
                 cur.execute('''COMMIT TRANSACTION;''')
                 conn.commit()
             except sqlite3.Error as e:
                 print(f'[{int(time.time()-STIME)}] SQLite3 Error: {e}')
                 sys.exit(1)
             if DEBUG:
-                print(f'[{int(time.time()-STIME)}] Job  done {height}')
+                print(f'[{int(time.time()-STIME)}] Index insert transaction done {height}')
+            cache_block.clear()
+            blkids.clear()
+            txids.clear()
+            addrids.clear()
+        else:
+            if DEBUG:
+                print(f'[{int(time.time()-STIME)}] Load done {height}', end='\r')
 
-            cache_block = {}
-            data_blkid = []
-            map_blkid = {}
-            data_txid = []
-            map_txid = {}
-            data_addrid = []
-            map_addrid = {}
-            data_blktime = []
-            map_blktime = {}
-            data_blktx = []
-            map_blktx = {}
-            data_txout = []
-            map_txout = {}
-            data_txin = []
-            map_txin = {}
+    cache_block = {}
+    cache_start = 0
+    cache_end = 0
+    blktime = []
+    blktx = []
+    txout = []
+    txin = []
+    for height in range(height_start, height_end+1):
+        if height not in cache_block.keys():
+            cache_start = height
+            cache_end = min(height_end+1, height+FLAGS.bulk)
+            if DEBUG:
+                print(f'[{int(time.time()-STIME)}] Cache update: {cache_start} ~ {cache_end-1}')
+            with multiprocessing.Pool(FLAGS.process) as p:
+                results = p.map(get_block_data, range(cache_start, cache_end))
+                for data in results:
+                    cache_block[data['height']] = data
+            if DEBUG:
+                print(f'[{int(time.time()-STIME)}] Cache update done')
+            cur.execute('''BEGIN TRANSACTION;''')
+        if cache_block[height]['height'] != height:
+            print(f'Something is wrong: {block["height"]} != {next_blkid}')
+            conn.close()
+            sys.exit(0)
+
+        cur.executemany('''INSERT INTO blktime (blk, miningtime)
+                             VALUES (?, ?);''', cache_block[height]['blktime'])
+        cur.executemany('''INSERT INTO blktx (blk, tx)
+                             VALUES (?, ?);''', cache_block[height]['blktx'])
+        cur.executemany('''INSERT INTO txout (tx, n, addr, btc)
+                             VALUES (?, ?, ?, ?);''', cache_block[height]['txout'])
+        cur.executemany('''INSERT INTO txin (tx, n, ptx, pn)
+                             VALUES (?, ?, ?, ?);''', cache_block[height]['txin'])
+
+        if height == cache_end-1:
+            if DEBUG:
+                print(f'[{int(time.time()-STIME)}] Ready to transaction {height}')
+            try:
+                cur.execute('''COMMIT TRANSACTION;''')
+                conn.commit()
+            except sqlite3.Error as e:
+                print(f'[{int(time.time()-STIME)}] SQLite3 Error: {e}')
+                sys.exit(1)
+            if DEBUG:
+                print(f'[{int(time.time()-STIME)}] Data insert transaction done {height}')
+            cache_block.clear()
+            blktime.clear()
+            blktx.clear()
+            txout.clear()
+            txin.clear()
         else:
             if DEBUG:
                 print(f'[{int(time.time()-STIME)}] Load done {height}', end='\r')
