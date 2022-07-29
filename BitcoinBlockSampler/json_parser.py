2ab84c80c81448979f45ec86cb259f44f8ee31d0
munhyunsu <munhyunsu@gmail.com>
2022-07-30 08:11:50 +0900

    json parser DEBUG message

--- BitcoinBlockSampler/json_parser.py
+++ BitcoinBlockSampler/json_parser.py
@@ -10,8 +10,11 @@ def addr_btc_from_vout(txid, vout):
         if 'address' in vout['scriptPubKey']: # compatibility Bitcoin-Core
             results.append((vout['scriptPubKey']['address'], float(vout['value'])))
         else:
-            for addr in vout['scriptPubKey']['addresses']:
-                results.append((addr, float(vout['value'])))
+            try:
+                for addr in vout['scriptPubKey']['addresses']:
+                    results.append((addr, float(vout['value'])))
+            except KeyError:
+                raise Exception(f'Exception {txid}\n{vout}')
     elif vout['scriptPubKey']['type'] in ('pubkey', 'nonstandard', 'nulldata'):
         try:
             for addr in [pubkey_to_address(get_pubkey(vout['scriptPubKey']['hex']))]:
