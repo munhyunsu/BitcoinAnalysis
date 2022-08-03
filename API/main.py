95bf66514b4abde58053fc5c6ef196501e3e4398
munhyunsu <munhyunsu@gmail.com>
2022-08-03 18:54:36 +0900

    Argument parsing

--- API/main.py
+++ API/main.py
@@ -1,6 +1,9 @@
+import os
 import sqlite3
 
 import secret
+
+FLAGS = _ = None
 
 
 def main():
@@ -8,5 +11,18 @@ def main():
 
 
 if __name__ == '__main__':
+    root_path = os.path.abspath(__file__)
+    root_dir = os.path.dirname(root_path)
+    os.chdir(root_dir)
+
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--debug', action='store_true',
+                        help='The present debug message')
+
+    FLAGS, _ = parser.parse_known_args()
+    DEBUG = FLAGS.debug
+
     main()
 
