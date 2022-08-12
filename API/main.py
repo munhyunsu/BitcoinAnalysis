6032c54455c34d5e8dd01080852822202a4ffa4d
munhyunsu <munhyunsu@gmail.com>
2022-08-13 06:23:11 +0900

    Cluster ID

--- API/main.py
+++ API/main.py
@@ -21,11 +21,12 @@ class Item(BaseModel):
 async def startup_event():
     global cur
     global conn
-    conn = sqlite3.connect(secret.path_service)
+    conn = sqlite3.connect(':memory:')
     cur = conn.cursor()
     cur.execute(f'''ATTACH DATABASE '{secret.path_index}' AS DBINDEX;''')
     cur.execute(f'''ATTACH DATABASE '{secret.path_core}' AS DBCORE;''')
     cur.execute(f'''ATTACH DATABASE '{secret.path_util}' AS DBUTIL;''')
+    cur.execute(f'''ATTACH DATABASE '{secret.path_service}' AS DBSERVICE;''')
     conn.commit()
 
 
@@ -36,6 +37,31 @@ async def shutdown_event():
     conn.close()
 
 
+@app.get('/', summary='Say hello to BitSQL API server')
+async def read_root():
+    return {'Say': 'Hello world!'}
+
+
+@app.get('/clusters/search', summary='Search cluster information')
+async def clusters_search(clustername: Union[str, None] = None):
+    response = []
+    global cur
+    global conn
+    if clustername is None:
+        return response
+    # TagID list
+    tagid_list = []
+    query = '''SELECT DBSERVICE.TagID.id
+               FROM DBSERVICE.TagID
+               WHERE DBSERVICE.TagID.tag = ?;'''
+    for row in cur.execute(query, (clustername,)):
+        tagid_list.append(row[0])
+
+    return response
+
+
+
+"""
 @app.get('/')
 async def read_root():
     global cur
@@ -54,4 +80,4 @@ async def read_item(item_id: int, q: Union[str, None] = None):
 @app.put('/items/{item_id}')
 async def update_item(item_id: int, item: Item):
     return {'item_name': item.name, 'item_id': item_id}
-
+"""
